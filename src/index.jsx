d6d85fbd3367957f9acbd253e8e6a7f02ee80aa1
Martin Tracey <matracey@outlook.ie>
2018-11-16 19:00:35 +0000

    Added css import for office-ui-fabric

--- src/index.jsx
+++ src/index.jsx
@@ -4,6 +4,7 @@ import ReactDOM from "react-dom";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 
+import "office-ui-fabric-react/dist/css/fabric.min.css";
 import "./index.css";
 import "./helpers.css";
 
