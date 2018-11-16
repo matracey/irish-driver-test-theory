a1fd5bb678c4a6f83915799d15a51b76ff5e3b30
Martin Tracey <matracey@outlook.ie>
2018-11-16 02:01:15 +0000

    Updates

---
+++ precache-manifest.97c69da8a76a1dd18c1a40363019bf7e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "4b4eafe977dd5cea21ea",
+    "url": "/static/js/main.4b4eafe9.chunk.js"
+  },
+  {
+    "revision": "ec3fc4f4232215e13ce5",
+    "url": "/static/js/1.ec3fc4f4.chunk.js"
+  },
+  {
+    "revision": "4b4eafe977dd5cea21ea",
+    "url": "/static/css/main.c75d33ab.chunk.css"
+  },
+  {
+    "revision": "e77489a8f7dac6213251a6c9240b0189",
+    "url": "/index.html"
+  }
+];
