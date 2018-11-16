2775195f9478ca712212e6b53cb744737b4703ae
Martin Tracey <matracey@outlook.ie>
2018-11-16 19:46:16 +0000

    Added logo to header.

--- src/components/Presenters/Header/Header.jsx
+++ src/components/Presenters/Header/Header.jsx
@@ -7,8 +7,9 @@ const Header = (props) => {
     const { appName, className } = props;
 
     return (
-            <span className="mh-1 ms-font-xl">{appName}</span>
         <header className={`${className ? `${className} ` : ""}w-100 d-flex justify-content-left align-items-center`}>
+            <div className="logo ml-1" />
+            <span className="ml-1 ms-font-xl">{appName}</span>
         </header>
     );
 };
